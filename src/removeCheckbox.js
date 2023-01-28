8684355dc35d13f226cc96f1a33d73070da20ff6
hafed <hafedrs@gmail.com>
2023-01-29 00:42:32 +0200

    modify remoe checkbox

--- src/removeCheckbox.js
+++ src/removeCheckbox.js
@@ -1,25 +1,29 @@
-export default class removeCheckbox {
-  constructor(toDoList) {
-    this.toDoList = toDoList;
-  }
-
-  removeCompletedTasks() {
+export default class RemoveCheckbox {
+constructor(toDoList) {
+this.toDoList = toDoList;
+}
+removeCompletedTasks() {
+    // Select all list items and create an array to store completed tasks
     const listItems = document.querySelectorAll('li');
     const completedTasks = [];
+
+    // Iterate through list items and check if checkbox is checked
     for (let i = 0; i < listItems.length; i += 1) {
-      const checkbox = listItems[i].querySelector('.checkbox');
-      if (checkbox.checked) {
-        completedTasks.push(i);
-        listItems[i].remove();
-      }
+        const checkbox = listItems[i].querySelector('.checkbox');
+        if (checkbox.checked) {
+            completedTasks.push(i);
+            listItems[i].remove();
+        }
     }
+
+    //Filter the task array, remove the completed tasks, update the task index and store in local storage
     this.toDoList.tasks = (
-      this.toDoList.tasks.filter((task, index) => !completedTasks.includes(index))
+        this.toDoList.tasks.filter((task, index) => !completedTasks.includes(index))
     );
     for (let i = 0; i < this.toDoList.tasks.length; i += 1) {
-      this.toDoList.tasks[i].index = i + 1;
+        this.toDoList.tasks[i].index = i + 1;
     }
     localStorage.setItem('tasks', JSON.stringify(this.toDoList.tasks));
     this.toDoList.populateList();
-  }
+}
 }
