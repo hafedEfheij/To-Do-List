dc7b1d3750ac1b0ace994e92614daaf54715feb8
hafed <hafedrs@gmail.com>
2023-01-28 23:14:47 +0200

    add updatie index

--- src/toDoList.js
+++ src/toDoList.js
@@ -11,7 +11,7 @@ class ToDoList {
     const task = {
       description,
       completed: false,
-      index: this.tasks.length,
+      index: this.tasks.length + 1,
     };
     this.tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
@@ -20,6 +20,9 @@ class ToDoList {
 
   removeTask(index) {
     this.tasks.splice(index, 1);
+    for(let i = index; i < this.tasks.length; i++) {
+      this.tasks[i].index = i+1;
+    }
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
     this.populateList();
   }
@@ -34,11 +37,14 @@ class ToDoList {
         listItems[i].remove();
       }
     }
-
     this.tasks = this.tasks.filter((task, index) => !completedTasks.includes(index));
+    for(let i = 0; i < this.tasks.length; i += 1) {
+      this.tasks[i].index = i+1;
+    }
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
     this.populateList();
   }
+
 
   editTask(index, newDescription) {
     if (index < this.tasks.length) {
@@ -50,8 +56,8 @@ class ToDoList {
 
   populateList() {
     this.list.innerHTML = '';
-    for (let i = 0; i < this.tasks.length; i += 1) {
-      const task = this.tasks[i];
+    for (let i = 1; i <= this.tasks.length; i += 1) {
+      const task = this.tasks[i-1];
       const listItem = document.createElement('li');
       listItem.innerHTML = `<input type="checkbox" class="checkbox"></input><p class="description">${task.description}</p><i class="fa-regular fa-trash-can"></i>`;
       listItem.addEventListener('click', (event) => {
@@ -60,7 +66,7 @@ class ToDoList {
           inputField.value = task.description;
           inputField.addEventListener('keyup', (event) => {
             if (event.code === 'Enter') {
-              this.editTask(i, inputField.value);
+              this.editTask(i-1, inputField.value);
               listItem.innerHTML = `<input type="checkbox" class="checkbox"></input><p class="description">${task.description}</p><i class="fa-regular fa-trash-can"></i>`;
             }
           });
@@ -69,11 +75,9 @@ class ToDoList {
           inputField.focus();
         }
       });
-
       listItem.querySelector('.fa-trash-can').addEventListener('click', () => {
-        this.removeTask(i);
+        this.removeTask(i-1);
       });
-
       this.list.addEventListener('change', (event) => {
         if (event.target.className === 'checkbox') {
           task.completed = event.target.checked;
