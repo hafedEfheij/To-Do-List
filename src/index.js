81b350b9e81fb57117841ce7edb404246f726380
hafed <hafedrs@gmail.com>
2023-01-28 23:32:59 +0200

    modify the index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
+// Import the ToDoList class from the toDoList.js file
 import ToDoList from './toDoList.js';
 
+// Create a new instance of the ToDoList class
 const toDoList = new ToDoList();
 
+// Add an event listener to the document that listens for a keyup event
 document.addEventListener('keyup', (event) => {
-  const inputField = document.getElementById('input-field');
-  if (event.code === 'Enter' && document.activeElement === inputField) {
-    toDoList.addTask(inputField.value);
-    toDoList.populateList();
-    inputField.value = '';
-  }
+// Get a reference to the input field
+const inputField = document.getElementById('input-field');
+
+// Check if the event code is 'Enter' and if the active element is the input field
+if (event.code === 'Enter' && document.activeElement === inputField) {
+// Add the task from the input field to the ToDoList
+toDoList.addTask(inputField.value);
+// Populate the list on the page with the updated tasks
+toDoList.populateList();
+// Clear the input field
+inputField.value = '';
+}
 });
 
+// Get a reference to the clear button
 const clearButton = document.getElementById('clear-button');
 
+// Add an event listener to the clear button that listens for a click event
 clearButton.addEventListener('click', () => {
-  toDoList.removeCompletedTasks();
+// Call the removeCompletedTasks method on the ToDoList instance to remove all completed tasks
+toDoList.removeCompletedTasks();
 });
