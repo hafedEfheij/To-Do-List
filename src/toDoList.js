e00a5ee4963ac7f440d35150ecfd1041984681af
hafed <hafedrs@gmail.com>
2023-01-29 01:18:17 +0200

    fix eslint

--- src/toDoList.js
+++ src/toDoList.js
@@ -1,15 +1,9 @@
-// This class creates a ToDoList object that allows users to add, remove, and view tasks.
 export default class ToDoList {
   constructor() {
-    // Get the tasks from local storage or create an empty array if none exist.
     this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    // Get the container element for the list and the list element itself.
     this.listContainer = document.getElementById('list-container');
-    this.list =
-      this.listContainer.querySelector('ul') || document.createElement('ul');
-    // Append the list element to the container.
+    this.list = this.listContainer.querySelector('ul') || document.createElement('ul');
     this.listContainer.appendChild(this.list);
-    // Populate the list with the existing tasks.
     this.populateList();
   }
 
@@ -20,7 +14,7 @@ export default class ToDoList {
       index: this.tasks.length + 1,
     };
     this.tasks.push(task);
-    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
     this.populateList();
   }
 
@@ -29,44 +23,44 @@ export default class ToDoList {
     for (let i = index; i < this.tasks.length; i += 1) {
       this.tasks[i].index = i + 1;
     }
-    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
     this.populateList();
   }
 
   editTask(index, newDescription) {
     if (index < this.tasks.length) {
       this.tasks[index].description = newDescription;
-      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
       this.populateList();
     }
   }
 
   populateList() {
-    this.list.innerHTML = "";
+    this.list.innerHTML = '';
     for (let i = 1; i <= this.tasks.length; i += 1) {
       const task = this.tasks[i - 1];
-      const listItem = document.createElement("li");
+      const listItem = document.createElement('li');
       listItem.innerHTML = `<input type="checkbox" class="checkbox"></input><p class="description">${task.description}</p><i class="fa-regular fa-trash-can"></i>`;
-      listItem.addEventListener("click", (event) => {
-        if (event.target.className === "description") {
-          const inputField = document.createElement("input");
+      listItem.addEventListener('click', (event) => {
+        if (event.target.className === 'description') {
+          const inputField = document.createElement('input');
           inputField.value = task.description;
-          inputField.addEventListener("keyup", (event) => {
-            if (event.code === "Enter") {
+          inputField.addEventListener('keyup', (event) => {
+            if (event.code === 'Enter') {
               this.editTask(i - 1, inputField.value);
               listItem.innerHTML = `<input type="checkbox" class="checkbox"></input><p class="description">${task.description}</p><i class="fa-regular fa-trash-can"></i>`;
             }
           });
-          listItem.innerHTML = "";
+          listItem.innerHTML = '';
           listItem.appendChild(inputField);
           inputField.focus();
         }
       });
-      listItem.querySelector(".fa-trash-can").addEventListener("click", () => {
+      listItem.querySelector('.fa-trash-can').addEventListener('click', () => {
         this.removeTask(i - 1);
       });
-      this.list.addEventListener("change", (event) => {
-        if (event.target.className === "checkbox") {
+      this.list.addEventListener('change', (event) => {
+        if (event.target.className === 'checkbox') {
           task.completed = event.target.checked;
         }
       });
